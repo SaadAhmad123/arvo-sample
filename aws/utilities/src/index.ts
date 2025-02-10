8e1703c00a6df4edf6e7bfb173961e23c501866f
Saad Ahmad <sahmad.bee15seecs@seecs.edu.pk>
2025-02-10 00:26:35 +0000

    update

--- aws/utilities/src/index.ts
+++ aws/utilities/src/index.ts
@@ -1,4 +1,11 @@
-export { eventBridgeToArvoEvent, arvoToEventBridgeEvent, isEventBridgeEvent } from './EventBridge.js';
+export {
+  eventBridgeToArvoEvent,
+  arvoToEventBridgeEvent,
+  isEventBridgeEvent,
+  AWSEventBridgePublishViolation,
+  sendToEventBus,
+} from './EventBridge.js';
+export type { AWSEventBusConfig } from './EventBridge.js';
 export { getRuntimeEnvironment } from './getRuntimeEnvironment.js';
 export type { RuntimeEnvironment } from './getRuntimeEnvironment.js';
 export { DynamoMachineMemory } from './DynamoMachineMemory.js';
